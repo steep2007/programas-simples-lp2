444fc6db6129546072c74e9d14361d19dbe77ab2
Stephani Borges <110248032+steep2007@users.noreply.github.com>
2023-01-12 13:06:02 -0400

    Delete tarefa-jay.py

--- tarefa-jay.py
+++
@@ -1,7 +0,0 @@
-letra = input('Digite sua letra')
-percor = len(letra)
-for i in range(0,percor):
-    if letra=='a' or letra=='e' letra=='i' or letra=='o' or letra=='u':
-        print('sua letra é uma vogal')
-    else:
-        print('sua letra é uma consoante')
